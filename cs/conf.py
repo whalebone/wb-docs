8c5d4c07832ec530978b088e6b595f6e9c0d516e
VSpurny <vladimir.spurny@whalebone.io>
2024-03-06 13:36:23 +0100

    commit

--- cs/conf.py
+++ cs/conf.py
@@ -134,7 +134,9 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
+html_css_files = [
+    'style.css',
+]
 
 html_logo = "whalebone.png"
 

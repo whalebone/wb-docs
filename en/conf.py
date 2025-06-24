b35f5aa5ce39e542218b220c7fd7aa28003dc4a5
Pavel Piskač <pavel.piskac@whalebone.io>
2025-06-24 11:56:41 +0200

    Update the HTML theme

--- en/conf.py
+++ en/conf.py
@@ -77,7 +77,7 @@ pygments_style = 'sphinx'
 todo_include_todos = False
 
 
-html_theme = "furo"
+html_theme = 'sphinx_rtd_theme'
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for

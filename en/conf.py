84d0ab5cea098da80621b5ee0d6b184c2a203e0d
Pavel Piskač <pavel.piskac@whalebone.io>
2025-06-24 13:21:23 +0200

    Fix the path to brand_variables.py

--- en/conf.py
+++ en/conf.py
@@ -25,7 +25,7 @@ import importlib.util
 
 # -- General configuration ------------------------------------------------
 
-brand_var_path = os.environ.get("BRAND_VAR_PATH", "../brand_variables.py")
+brand_var_path = os.environ.get("BRAND_VAR_PATH", "../../brand_variables.py")
 
 spec = importlib.util.spec_from_file_location("brand_variables", brand_var_path)
 brand_variables = importlib.util.module_from_spec(spec)

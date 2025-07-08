c6b85799d2d79f7af3d53c90e236076d388c8c9e
Pavel Piskač <pavel.piskac@whalebone.io>
2025-07-08 15:21:35 +0200

    Add index specific to the AUPDNS project.

--- en/conf.py
+++ en/conf.py
@@ -88,7 +88,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'index_immunity.rst', 'index_peacemaker.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'index_immunity.rst', 'index_peacemaker.rst', 'index_aupdns.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
